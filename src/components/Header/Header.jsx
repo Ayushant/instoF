71723549cd2078e6b313a039cd2b41c1b3bf3b7f
Ayushant Khandekar <120311510+Ayushant@users.noreply.github.com>
2024-07-03 20:59:48 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,20 +7,34 @@ export class Header extends Component {
       <header>
        
        
- <div className=' h-[460px] rounded-tl-[500px] rounded-tr-[500px] bg-amber-200 '>
+ <div className=' h-[460px] rounded-tl-[500px] rounded-tr-[500px] bg-amber-200 sm:bg-amber-200 md:bg-amber-200'>
 
     <div className='pb-8  '>
-    <h1 className=' text-orange-500 pt-24 text-4xl text-center font-bold sm:text-8xl' >DON'T MISS OUT</h1>
-    <p className='text-center text-xl mt-7 text-dark sm:mt-5'>Sign up for our newsletter about trending menus and new restaurants</p>
+    <h1 className='text-orange-500 pt-24 text-4xl text-center font-bold sm:text-8xl' >DON'T MISS OUT</h1>
+    <p className='text-center text-xl mt-5 text-dark'>Sign up for our newsletter about trending menus and new restaurants</p>
+    </div>
+
+
+    <div className='xl:   flex justify-center gap-8 xl:mt-3'>
+      <form action="">
+        <label class='block'>
+          <input class='bg-black rounded-full text-white p-2 ml-2 w-[29vb] xl:w-[60vb] xl:mt-5 xl:p-2 xl:bg-black xl:'  placeholder='Enter email' />
+        </label>
+      </form>
+
+      <div>
+        <button class="bg-orange-600 text-white p-2 w-[22vb] rounded-full xl:mt-5">
+         Sign up
+        </button>
+      </div>
     </div>
     
 
-   <div className='flex justify-center mt-3'> 
-
+   {/* <div   className='hidden sm:block   flex justify-center  mt-3  '> 
     <form className='  '>
   <label class="block">
     
-    <input class="w-[autovb] mt-2 mx-auto mr- rounded-full border-slate-200 p-3 px-6 bg-black text-white placeholder-white sm:w-[autovb] sm:mt-10 sm:p-2 sm:px-5 sm:mx-auto" placeholder='Enter Email'/>
+    <input class="w-[60vb] mt-10 rounded-full border-slate-200 p-2 px- bg-black text-white placeholder-white" placeholder='Enter Email'/>
     
     <p class="mt-2 opacity-10 contrast-more:opacity-100 text-white text-sm">
      
@@ -29,14 +43,13 @@ export class Header extends Component {
 </form>
   
 
-    <div className=' '>
-    <button class="bg-orange-500 mt-1 w-auto hover:bg-orange-500 text-white font-semibold py-5 px-4  ml-2 rounded-full hover:bg-orange-600
-        sm:mt-10 sm:py-2 sm:w-36 ">
-  Sign Up
+    
+    <button class="bg-orange-500 mt-10 w-36 hover:bg-orange-500 text-white font-semibold py-2 px-4  ml-2 rounded-full hover:bg-orange-600">
+  Sign up
 </button>
-</div>
 
-</div> 
+
+</div>  */}
 
 </div> 
 
@@ -46,7 +59,6 @@ export class Header extends Component {
 }
 
 export default Header
-
 
 
 
