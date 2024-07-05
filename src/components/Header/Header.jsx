bdcb4ad6b2fff82f35a74bef9b5a33fa5db0bf0f
Ayushant Khandekar <120311510+Ayushant@users.noreply.github.com>
2024-07-05 21:07:37 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,15 +15,15 @@ export class Header extends Component {
     </div>
 
 
-    <div className=' xl:  flex justify-center  gap-6 xl:mt-3 sm:gap-4 sm:mb-'>
+    <div className='  flex justify-center   gap-4 xl:mt-3 sm:gap-9  '>
       <form action="">
         <label class='block'>
-          <input class='bg-black rounded-full text-white p-1 sm:p-2  xl:ml-2 w-[29vb] xl:w-[60vb] xl:mt-5 xl:p-2 xl:bg-black sm:w-[10vb] sm:ml-20 sm:px-10'  placeholder='Enter email' />
+          <input class='bg-black rounded-full text-white p-1 sm:p-2  xl:ml-2 w-[29vb] xl:w-[60vb] xl:mt-5 xl:bg-black '  placeholder='Enter email' />
         </label>
       </form>
 
       <div>
-        <button class="bg-orange-600  text-white p-1 sm:p-2 w-[20vb] xl:w-[22vb] rounded-full xl:mt-5 sm:w-[10] ">
+        <button class="bg-orange-600  text-white p-1  sm:p-2  w-[20vb] xl:w-[22vb] rounded-full xl:mt-5 sm:w-[10]   ">
          Sign up
         </button>
       </div>
